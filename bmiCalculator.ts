f182d2c8912275b18836b818f568feeb5a54930b
Miguel Guerrero <miguel.guerrerog6@gmail.com>
2025-12-06 23:56:40 -0500

    9.5: WebBMI

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -3,34 +3,24 @@ interface Values {
     estatura: number;
 }
 
-const parseArguments = (args: string[]): Values => {
-    if (args.length < 4) throw new Error('Not enough arguments');
-    if (args.length > 4) throw new Error('Too many arguments');
-
-    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-        return {
-            peso: Number(args[2]),
-            estatura: Number(args[3])
-        };
+export const parseArguments = (a: string, b: string): Values => {
+    if (isNaN(Number(a)) || isNaN(Number(b))) {
+        throw new Error('Bad arguments');
     }
+
+    return {
+        peso: Number(b),
+        estatura: Number(a)
+    };
 }
 
-const calculate = (masa: number, altura: number): string => {
+export const calculateBmi = (masa: number, altura: number): string => {
     const alturaMetro: number = altura / 100;
     const imc: number = masa / Math.pow(alturaMetro, 2);
     if (imc < 18.5) return 'Peso bajo ';
-    if (imc >= 18.5 && imc < 24.9) return 'Peso normal';
+    if (imc >= 18.5 && imc < 24.9) return 'Normal (healthy weight)';
     if (imc >= 24.9 && imc < 29.9) return 'Sobrepeso';
-    if (imc > 30) return 'Obesidad';
-}
+    if (imc >= 29.9) return 'Obesidad';
 
-try {
-    const { peso, estatura } = parseArguments(process.argv);
-    console.log(calculate(peso, estatura));
-} catch (error: unknown){
-    let errorMessage = 'Something bad happened.';
-    if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message;
-  }
-  console.log(errorMessage);
+    return 'Algo salio mal';
 }
--- index.ts
+++ index.ts
@@ -1,12 +1,36 @@
 import express from 'express';
+import { calculateBmi, parseArguments } from './bmiCalculator';
+//import qs from 'qs';
 
 const app = express();
 
-app.get('/hello', (req, res) => {
-    res.send('Hello Full Stack!');
+app.get('/hello', (_req, res) => {
+    res.status(200).send('Hello Full Stack!');
 });
 
-const PORT = process.env.PORT;
+app.get('/bmi', (req, res) => {
+    const height = req.query.height as string;
+    const weight = req.query.weight as string;
+
+    if (!height || !weight) {
+        res.status(400)
+            .json({ error: 'Faltan parametros: height y weight son requeridos' });
+    }
+
+    try {
+        const { peso, estatura } = parseArguments(height, weight);
+        const imc = calculateBmi(peso, estatura);
+        res.status(200).json({
+            height: estatura,
+            weight: peso,
+            bmi: imc
+        });
+    } catch (error) {
+        res.status(400).json({ error: 'Malformatted parameters' });
+    }
+});
+
+const PORT = 3003;
 
 app.listen(PORT, () => {
     console.log(`Server starting on port: ${PORT}`);
